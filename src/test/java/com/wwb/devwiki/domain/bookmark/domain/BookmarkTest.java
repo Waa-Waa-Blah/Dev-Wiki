8c0d45043c27358683d22424695c8ee29af9f4e5
qospwmf <qospwmf@gmail.com>
2022-07-24 19:35:48 +0900

    [DW-9]⛑️test : 도메인 테스트 코드 작성

--- src/test/java/com/wwb/devwiki/domain/bookmark/domain/BookmarkTest.java
+++ src/test/java/com/wwb/devwiki/domain/bookmark/domain/BookmarkTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.wwb.devwiki.domain.bookmark.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 class BookmarkTest {
-  
+
+    @Test
+    @DisplayName("북마크 생성 검증")
+    public void BookmarkCreateValid() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
 }
