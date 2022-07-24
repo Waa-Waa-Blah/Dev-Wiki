0c3dce27d734a92eeacfde3d1b7550d303775d84
qospwmf <qospwmf@gmail.com>
2022-07-24 16:47:47 +0900

    [DW-7]🏗️ build : Project init

---
+++ src/main/java/com/wwb/devwiki/DevwikiApplication.java
@@ -0,0 +1,13 @@
+package com.wwb.devwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevwikiApplication.class, args);
+	}
+
+}
