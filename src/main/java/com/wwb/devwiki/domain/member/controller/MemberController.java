7e8e0af769764f45c692a104465a8e4c3a0f35ca
Yunsik-Choi <choiys0311@gmail.com>
2022-07-30 15:59:05 +0900

    [DW-18]⚡feat : Member login 기능 개발

--- src/main/java/com/wwb/devwiki/domain/member/controller/MemberController.java
+++ src/main/java/com/wwb/devwiki/domain/member/controller/MemberController.java
@@ -1,12 +1,17 @@
 package com.wwb.devwiki.domain.member.controller;
 
+import com.wwb.devwiki.domain.member.domain.Member;
 import com.wwb.devwiki.domain.member.dto.MemberJoinReqDto;
+import com.wwb.devwiki.domain.member.dto.MemberLoginReqDto;
 import com.wwb.devwiki.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/member")
@@ -20,5 +25,11 @@ public class MemberController {
         memberService.join(memberJoinReqDto);
     }
 
+    @PostMapping("/login")
+    public void login(MemberLoginReqDto memberLoginReqDto, HttpServletRequest httpServletRequest) {
+        HttpSession session = httpServletRequest.getSession();
+        Member member = memberService.login(memberLoginReqDto.nickname, memberLoginReqDto.password);
+        session.setAttribute("loginUser", member.getId());
+    }
 
 }
